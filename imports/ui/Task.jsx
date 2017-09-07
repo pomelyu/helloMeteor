f2587133309ae19f70e2a5db1d0908f57a50e197
pomelyu <pomelyu5199@icloud.com>
2017-09-07 17:22:59 +0800

    implment remove and check feature

--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -1,8 +1,46 @@
 import React, { PropTypes } from 'react';
 
-const Task = (props) => (
-  <li>{props.task.text}</li>
-);
+import Tasks from '../api/tasks.js';
+
+class Task extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.toggleChecked = this.toggleChecked.bind(this);
+    this.deleteThisTask = this.deleteThisTask.bind(this);
+  }
+
+  toggleChecked() {
+    Tasks.update(this.props.task._id, {
+      $set: { checked: !this.props.task.checked },
+    })
+  }
+
+  deleteThisTask() {
+    Tasks.remove(this.props.task._id);
+  }
+
+  render() {
+    const taskClassName = (this.props.task.checked) ? 'checked' : '';
+
+    return (
+      <li className={taskClassName}>
+        <button className="delete" onClick={this.deleteThisTask}>
+          &times;
+        </button>
+
+        <input
+          type="checkbox"
+          readOnly
+          checked={this.props.task.checked}
+          onClick={this.toggleChecked}
+        />
+
+        <span className="text">{this.props.task.text}</span>
+      </li>
+    );
+  }
+}
 
 Task.propTypes = {
   task: PropTypes.object.isRequired,
